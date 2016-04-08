4af6ce47c7db8fc3a3f4e18333d8f5aca7670a04
IllusionistAR <keeganspraggs@gmail.com>
2016-04-08 18:27:32 +1200

    added detail html for products

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'cart',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -103,3 +104,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+# cart session information
+
+CART_SESSION_ID = 'cart'
