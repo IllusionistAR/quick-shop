109416a38e53be669c1064038cd57594467f5627
IllusionistAR <keeganspraggs@gmail.com>
2016-05-23 14:44:22 +1200

    migrated css to Skeleton Responsive Framework

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from .tasks import order_created
 
 # Create your views here.
 
@@ -16,6 +17,8 @@ def order_create(request):
                                         price=item['price'], quantity=item['quantity'])
             # clear the cart
             cart.clear()
+            # launching asynchronous task
+            order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
 
     else:
